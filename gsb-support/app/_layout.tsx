e335659b16968bbd6e0ac18467c0175e75edec57
irenearchimbaud <irenearchimbaud@gmail.com>
2025-06-17 15:00:53 +0200

    ajout de l'index

---
+++ gsb-support/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Slot } from "expo-router";
+import { AuthProvider } from "../hooks/useAuth";
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Slot />
+    </AuthProvider>
+  );
+}
---
+++ gsb-support/app/index.tsx
@@ -0,0 +1,10 @@
+import { useAuth } from "../hooks/useAuth";
+import { Redirect } from "expo-router";
+
+export default function Index() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
+
+  return user ? <Redirect href="/(app)/dashboard" /> : <Redirect href="/(auth)/login" />;
+}
